902414c05f0c486c6748cf2edfb8d47a43cba2e1
hinsonan <hinsonan@gmail.com>
2017-02-20 14:39:55 -0600

    makes the array based on user input

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,9 +1,39 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class QuickSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		//Ask the user for the size of the array they want
+		System.out.print("Enter the size of the array you want");
+		int size = scan.nextInt();
+		
+		int[] unSortedArray = createIntArray(size);
+		
+		HoarePartition(unSortedArray);
+		
 
+	}
+	
+	private static void HoarePartition(int[] arr)
+	{
+		int p = arr[0];
+		
+		
+	}
+	
+	private static int[] createIntArray(int size){
+		int[] array = new int[size];
+		
+		//this loop will fill the array with values
+		for(int i = 0; i < array.length; i++){
+			//put random numbers into the array
+			Random rand = new Random();
+			//makes the random numbers 5 times the size of the array the user wants
+			array[i] = rand.nextInt((size*10)) + 1;
+		}
+		return array;
 	}
 
 }
