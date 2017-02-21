2505e1ce60da5eaf8fe09ad80af62f015183bedd
hinsonan <hinsonan@gmail.com>
2017-02-21 14:29:10 -0600

    revert back

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -9,11 +9,30 @@ public class QuickSort {
 		Scanner scan = new Scanner(System.in);
 		//Ask the user for the size of the array they want
 		System.out.print("Enter the size of the array you want");
+<<<<<<< HEAD
 		int size = scan.nextInt();
+=======
+		size = scan.nextInt();
+		
+		int[] array = createIntArray(size);
+		System.out.println("Unsorted Array");
+		for(int n: array ){
+			System.out.print(n + " ");
+		}
+>>>>>>> parent of 6a23d49... implements quicksort but have out of bounds error
 		
 		int[] unSortedArray = createIntArray(size);
 		
+<<<<<<< HEAD
 		HoarePartition(unSortedArray);
+=======
+		HoarePartition(array);
+		
+		System.out.println("Partitioned Array");
+		for(int n: array ){
+			System.out.print(n + " ");
+		}
+>>>>>>> parent of 6a23d49... implements quicksort but have out of bounds error
 		
 
 	}
@@ -21,6 +40,42 @@ public class QuickSort {
 	private static void HoarePartition(int[] arr)
 	{
 		int p = arr[0];
+<<<<<<< HEAD
+=======
+		int i = 0;
+		int j = (size-1) + 1;
+		
+		do
+		{
+			while(arr[i] <= p)
+			{
+				i = i + 1;
+			}
+			
+			
+			do
+			{
+				j = j - 1;
+			}
+			while(arr[j] >= p);
+			//swap arr[i], arr[j]
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+			
+		}
+		while(!(i >= j));
+		//undo the last swap
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+		
+		//swap the partition value
+		temp = arr[0];
+		arr[0] = arr[j];
+		arr[j] = temp;
+	
+>>>>>>> parent of 6a23d49... implements quicksort but have out of bounds error
 		
 		
 	}
